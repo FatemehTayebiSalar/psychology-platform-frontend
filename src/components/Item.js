cd16c8c0f7fb2c3f3938ee9ddad7f8df50af147d
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2022-05-22 09:33:22 +0430

    error fixed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,23 @@
-import React from 'react';
-//import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import styles from "./Item.module.css";
 
-const Item = (props) => {
-    return (
-        <div className={styles.container}>
-            <img src={props.image} alt="Items" />
-            <h3><a>{props.title}</a></h3>
-        </div>
-    );
-};
+
+
+
+class Item extends Component {
+
+    
+    render() {
+        
+        const {image , title ,id} = this.props;
+        return (
+            <div className={styles.container}>
+                <img src={image} alt="Items" />
+                <h3><Link to={`/products/${id}`}>{title}</Link></h3>
+            </div>
+        );
+    }
+}
 
 export default Item;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,24 +1,33 @@
-import React ,{useState} from 'react';
+import React, { Component } from 'react';
+
 import styles from "./Items.module.css";
-import Item from './Item';
+import Card from './Item';
+
 
 import Podcast from "../images/Podcast.png";
 import Events from "../images/Events.png";
 import Psychologists from "../images/Psychologists.png";
 import Meditation from "../images/Meditation.png";
 
-const Items = () => {
-    const data = useState(
-        {id:1,image:Podcast,title:"گوش کنیم"},
-        {id:2,image:Meditation, title:"آرام شویم"},
-        {id:3,image:Psychologists, title:"حرف بزنیم" },
-        {id:4,image:Events, title:"شرکت کنیم" })
-        
-    return (
-        <div className={styles.container}>
-               {data.map(item => <Item key={data.id} id={data.id} image={data.image} title={data.title}/>)}
-        </div>
-    );
-};
+class Items extends Component {
+    constructor(){
+        super();
+        this.state={
+            Data:[
+                {id:1,image:Podcast,title:"گوش کنیم"},
+                {id:2,image:Meditation, title:"آرام شویم"},
+                {id:3,image:Psychologists, title:"حرف بزنیم" },
+                {id:4,image:Events, title:"شرکت کنیم" }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className={styles.container}>
+               {this.state.Data.map(item => <Card id={item.id} key={item.id} image={item.image} title={item.title}/>)}
+            </div>
+        );
+    }
+}
 
 export default Items;
