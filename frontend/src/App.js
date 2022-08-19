286b2fa7ff0cf26acfa094582e446e7df10024b9
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2022-08-19 23:00:27 +0430

    Worked on Banner component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import PsychDetail from './components/PsychDetail.js';
 
 function App() {
   return (
-    <div style={{backgroundColor: "#F6F6F6"}} >
+    <div style={{backgroundColor: "#dedcdc"}} >
       
        <Navbar />
        <Routes>
--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./Banner.module.css";
 import bannerPsych from "../images/bannerPsych.jpg";
 import bannerPod from "../images/bannerPod.jpg";
+import playIcon from "../images/Icons/play.png";
 
 const Banner = () => {
     return (
@@ -11,8 +12,11 @@ const Banner = () => {
                 <h2>همراه شما با یاری بهترین</h2>
                 <h2 className={styles.hL3}>روانشناسان</h2>
                 <div className={styles.detail}>
-                     <span>We live in a world of knitting</span>
-                     <div><img src={bannerPod} alt="Banner" /></div>
+                    <span>We live in a world of knitting hhvgkhkjhgkhjgkjhgkhgkjhgjkhg</span>
+                    <div className={styles.podImgContainer}>
+                        <img src={bannerPod} className={styles.podcastImg} alt="Banner" />
+                        
+                    </div>
                 </div>
             </div>
             <img src={bannerPsych} alt="Banner" className={styles.psychImg}/>
