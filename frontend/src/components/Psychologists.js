857a028dcafdc0387782d2472620eb6dde9c5a3a
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2023-01-16 22:46:50 +0330

    psychologists page was connected to backend

--- frontend/src/components/Psychologists.js
+++ frontend/src/components/Psychologists.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import Item from './Item';
-import PsychData from './PsychData.js';
-import styles from './Psychologists.module.css'
+import styles from './Psychologists.module.css';
+import axios from 'axios';
 
 
 const Psychologits = () => {
+    const [psychsData , setPsychsData] = useState([{id: "" , name : "" , degree : "" , imageURL : ""}])
+
+    useEffect(()=>{
+        axios.get("http://localhost:5000/psychologists")
+        .then(response => {
+            const data = response.data.data.psychologists
+            setPsychsData(data)
+        }
+        ).catch(errors => console.log(errors))
+        
+    },[])
+
     return (
         <div className={styles.container}>
             <h1>درمانگران</h1>
             <div className={styles.itemContainer}>
-                {PsychData.map(item => <Item size={"250px"} id={item.id} key={item.id} image={item.image} title={item.name}/>)}
+                {psychsData.map(item => <Item size={"250px"} id={item.id} key={item.id} image={item.imageURL} title={item.name}/>)}
             </div>
         </div>
     );
