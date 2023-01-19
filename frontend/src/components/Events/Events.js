10128ec2fa81c0c72026f470e0e02c5d672de159
Fatemeh TayebiSalar <fatemehtayebi98@gmail.com>
2023-01-19 20:35:34 +0330

    events page was created and connected to backend

---
+++ frontend/src/components/Events/Events.js
@@ -0,0 +1,32 @@
+import React, {useState , useEffect} from 'react';
+
+import styles from './Events.module.css';
+import axios from 'axios';
+import EventsItem from './EventsItem';
+
+
+
+const Events = () => {
+    const [eventsData , setEventsData] = useState([])
+
+    useEffect(()=>{
+        axios.get("http://localhost:5000/events")
+        .then(response => {
+            const data = response.data.data.events
+            setEventsData(data)
+        }
+        ).catch(errors => console.log(errors))
+        
+    },[])
+
+    return (
+        <div className={styles.container}>
+            <h3 className={styles.title}>رویدادها</h3>
+            <div className={styles.itemContainer}>
+                {eventsData.map(item => <EventsItem title={item.title} id={item.id} key={item.id} image={item.imageURL} date={item.date} capacity={item.capacity}/>)}
+            </div>
+        </div>
+    );
+};
+
+export default Events;
---
+++ frontend/src/components/Events/EventsItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import capacityIcon from '../../images/Icons/capacity.png';
+import registeredIcon from '../../images/Icons/registered.png';
+import PersianNumber from '../shared/PersianNumber';
+import styles from './EventsItem.module.css';
+
+
+
+
+class EventItem extends Component {
+
+    
+    render() {
+        const {image , title ,id , capacity,date} = this.props;
+
+        
+        
+        return (
+            <div className={styles.container}>
+                <div className={styles.info}>
+                    <img src={image} alt="EventImage" className={styles.eventImg} />
+                    <span className={styles.text}>
+                        <h4><Link to={`/`}>{title}</Link></h4>
+                        <p>{date}</p>
+                    </span>
+                </div>
+                
+                <div className={styles.icons}>
+                    <span className={styles.capacity}>
+                        <span><PersianNumber number = {capacity} /></span>
+                        <img src={capacityIcon} alt="capacity" /> 
+                    </span>
+                    <span className={styles.registered}>
+                        <span><PersianNumber number = {10} /></span>
+                        <img src={registeredIcon} alt="registered" />
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EventItem;
